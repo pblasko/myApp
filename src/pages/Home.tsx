5b925411756323484ae2033944cb5e5efba193db
peterblasko <blasko.peter.hu@gmail.com>
2024-10-11 22:04:07 +0200

    Xcode is ready

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>myApp Posta</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
