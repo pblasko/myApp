df39c5336999a437afdc79b8ce1d75143180f6e0
peterblasko <blasko.peter.hu@gmail.com>
2024-10-12 22:56:38 +0200

    Create basic design to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { logInOutline, personCircleOutline } from 'ionicons/icons';
+import MPL from '../assets/magyar-posta-logo.svg';
 
 const Login: React.FC = () => {
+    const doLogin = (event: any) => {
+        event.preventDefault();
+        console.log('do Login');
+    }
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color="primary">
+        <IonToolbar color={'primary'}>
           <IonTitle>myApp Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        Posta Garage Login Page
+      <IonContent scrollY={false}>
+        <IonCard>
+            <div className='ion-text-center ion-padding'>
+                <img src={MPL} alt='Posta logo' width={'50%'} />
+            </div>
+            <IonCardContent>
+                <form onSubmit={doLogin}>
+                    <IonInput fill='outline' labelPlacement='floating' label='e-mail' type='email' placeholder='peter@posta.hu'></IonInput>
+                    <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='password' type='password'></IonInput>
+                    <IonButton type='submit' expand='block' className='ion-margin-top'>
+                        Login
+                        <IonIcon icon={logInOutline} slot="end" />
+                    </IonButton>
+                    <IonButton routerLink='/register' color={'secondary'} type='button' expand='block' className='ion-margin-top'>
+                        Create Account
+                        <IonIcon icon={personCircleOutline} slot="end" />
+                    </IonButton>
+                </form>
+            </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
