04e252766c9fe8b802486cfb98096f3bd517ba13
peterblasko <blasko.peter.hu@gmail.com>
2024-10-14 19:33:02 +0200

    Responsive register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { IonBackButton, IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { IonBackButton, IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonPage, IonRow, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
 import React from 'react';
 import { checkmarkDoneOutline } from 'ionicons/icons';
 
@@ -22,18 +22,24 @@ const Register: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent scrollY={false}>
-        <IonCard>
-            <IonCardContent>
-                <form onSubmit={doRegister}>
+        <IonGrid fixed>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size='12' sizeMd='8' sizeLg='6' sizeXl='4'>
+              <IonCard>
+                <IonCardContent>
+                  <form onSubmit={doRegister}>
                     <IonInput fill='outline' labelPlacement='floating' label='e-mail' type='email' placeholder='peter@posta.hu'></IonInput>
                     <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='password' type='password'></IonInput>
                     <IonButton type='submit' expand='block' className='ion-margin-top' color={'secondary'}>
-                        Register
-                        <IonIcon icon={ checkmarkDoneOutline } slot="end" />
+                      Register
+                      <IonIcon icon={ checkmarkDoneOutline } slot="end" />
                     </IonButton>
-                </form>
-            </IonCardContent>
-        </IonCard>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
