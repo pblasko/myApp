3a43a24c4637c49d62162741b66d935323156ad4
peterblasko <blasko.peter.hu@gmail.com>
2024-10-20 21:33:45 +0200

    List items loading

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,21 +1,48 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { IonAvatar, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonChip, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonMenuButton, IonPage, IonRefresher, IonRefresherContent, IonSearchbar, IonSkeletonText, IonTitle, IonToolbar, useIonAlert, useIonToast, useIonViewWillEnter } from '@ionic/react';
+import { trashBinOutline } from 'ionicons/icons';
 import React, {useState} from 'react';
 
 const List: React.FC = () => {
   const [users, setUsers] = useState<any []>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [showAlert] = useIonAlert();
+  const [showToast] = useIonToast();
   useIonViewWillEnter(() => {
     const fetchUsers = async () => {
       const users = await getUsers();
       console.log(users);
       setUsers(users);
+      setLoading(false);
     };
     fetchUsers();
   });
   const getUsers = async () => {
     const data = await fetch ('https://randomuser.me/api?results=10');
     const users = await data.json();
-    return users;
+    return users.results;
+  }
+  const clearList = () => {
+    showAlert({
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete all users?',
+      buttons: [
+      {text: 'Cancel', role:'cancel'},
+      {text: 'Delete', handler: () => {
+        setUsers([]);
+        showToast({
+          message: 'All users deleted',
+          duration: 2000,
+          color: 'danger'
+        })
+      }}
+      ]
+    })
+  };
+  const doRefresh = async(event:any) => {
+    const data = await getUsers();
+    setUsers(data);
+    setLoading(false);
+    event.detail.complete();
   }
   return (
     <IonPage>
@@ -25,10 +52,58 @@ const List: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>List</IonTitle>
+          <IonButtons slot='end'>
+            <IonButton onClick={clearList}>
+              <IonIcon slot='icon-only' icon={trashBinOutline} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+        <IonToolbar color={'success'}>
+          <IonSearchbar />
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
-        List items
+      <IonContent>
+        <IonRefresher slot='fixed' onIonRefresh={(ev) => doRefresh(ev)}>
+          <IonRefresherContent />
+        </IonRefresher>
+        {loading && 
+          [...Array(10)].map((_, index) => (
+            <IonCard key={index}>
+              <IonCardContent className='ion-no-padding'>
+                <IonItem lines='none'>
+                  <IonAvatar slot='start'>
+                    <IonSkeletonText />
+                  </IonAvatar>
+                  <IonLabel>
+                    <IonSkeletonText animated style={{width: '150px'}} />
+                    <p><IonSkeletonText /></p>
+                  </IonLabel>
+                  <IonChip slot='end' color={'primary'}>
+                    
+                  </IonChip>
+                </IonItem>
+              </IonCardContent>
+            </IonCard>
+          ))
+        }
+        {users.map((user, index) => (
+          <IonCard key={index}>
+            <IonCardContent className='ion-no-padding'>
+              <IonItem lines='none'>
+                <IonAvatar slot='start'>
+                  <IonImg src={user.picture.large} />
+                </IonAvatar>
+                <IonLabel>
+                  {user.name.first} {user.name.last}
+                  <p>{user.email}</p>
+                </IonLabel>
+                <IonChip slot='end' color={'primary'}>
+                  {user.nat}
+                </IonChip>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+        ))}
       </IonContent>
     </IonPage>
   );
